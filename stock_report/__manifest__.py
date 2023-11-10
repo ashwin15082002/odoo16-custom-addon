b12d2503867b39113b79378af7ec105df6ac2244
Ashwin <ashwin15082002@gmail.com>
2023-11-10 10:27:30 +0530

    stock report

---
+++ stock_report/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Spotter Sale Order",
+    'version': '16.0.4.0.0',
+    'depends': ['sale'],
+    'author': "Ashwin",
+    'category': 'Category',
+    'description': """   """,
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+    # data files always loaded at installation
+    'data': [
+        'views/sale_order_views.xml',
+    ],
+
+}
