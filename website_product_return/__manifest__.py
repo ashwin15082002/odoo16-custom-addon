869b03d1b513c200e5816e64d9e6c7aab03faa94
Ashwin <ashwin15082002@gmail.com>
2023-12-11 17:38:01 +0530

    ongoing website_product_return

---
+++ website_product_return/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Multi Product Return",
+    'version': '16.0.4.0.0',
+    'depends': ['sale','website'],
+    'author': "Ashwin",
+    'category': 'category',
+    'description': """  """,
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+    # data files always loaded at installation
+    'data': [
+        'views/portal_views.xml',
+    ],
+}
